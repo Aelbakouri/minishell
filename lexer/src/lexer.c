82483368ec5b2c0be383d38cb0e9eeb60b80fff3
Allal El Bakouri <ael-bako@e1r8p8.1337.ma>
2023-04-10 03:23:51 +0000

    just a commit

--- lexer/src/lexer.c
+++ lexer/src/lexer.c
@@ -34,7 +34,7 @@ void lexer_skip_whitespace(lexer_T* lexer)
 
 token_T* lexer_get_next_token(lexer_T* lexer)
 {
-    while (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
+    while (lexer->c && lexer->i < strlen(lexer->contents))
     {
         if (lexer->c == ' ' || lexer->c == 10)
             lexer_skip_whitespace(lexer);
